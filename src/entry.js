704681b987050ef49e70d520517c5d623fcc4724
PatOmation <patomation@gmail.com>
2017-09-29 10:42:00 -0700

    canvas example added

--- src/entry.js
+++ src/entry.js
@@ -14,19 +14,47 @@ import image from '../images/CHAT2.gif'
 import svg from '../images/logout.svg'
 
 var App = React.createClass({
+    componentDidMount:function () {
+        var canvas = document.getElementById("workSpace");
+        var ctx = canvas.getContext("2d");
+        let x = 20,
+            y = 30;
 
-  render: function() {
-    return(
-      <div className='app'>
 
-        Testing! Maybe Kill Patrick??
+        function draw(){
+            ctx.clearRect(0,0,800,600)
+            ctx.fillRect(x,y,150,100);
+        }
 
-        <canvas id={"myCanvas"} width={"200"} height={"100"} style={{"border":"1px solid #000000"}}>
-        </canvas>
+        setInterval(draw,10);
 
-      </div>
-    )
-  }
+        canvas.onmousedown = function(e){
+            console.log('mouse down');
+            x = e.pageX - canvas.offsetLeft;
+            y = e.pageY - canvas.offsetTop;
+        }
+        canvas.onmouseup = function(e){
+            console.log('mouse up');
+        }
+
+        canvas.onmousemove = function(e){
+        
+        }
+    },
+
+    render: function() {
+        return(
+            <div className='app'>
+
+                Testing! Maybe Kill Patrick??<br/><br/>
+
+                <canvas id={"workSpace"} width={"800"} height={"600"} style={{"border":"3px solid #000000"}}>
+                </canvas>
+
+
+            </div>
+        )
+    }
 });
 
 ReactDOM.render(
