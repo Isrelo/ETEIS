a8ff655d832721950eb78edd18df410295105405
PatOmation <patomation@gmail.com>
2017-09-28 20:52:48 -0700

    first commit of junk that we dont necissarily need

---
+++ src/components/layout/spinner.js
@@ -0,0 +1,14 @@
+//Modules
+import React from "react";
+
+export default React.createClass({
+
+  render: function() {
+    return (
+      <div
+        className="spinner verticalAlign">
+
+      </div>
+    );
+  },
+});
